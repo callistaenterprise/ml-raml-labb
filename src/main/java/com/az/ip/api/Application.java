542242a231aa3e542a37cd0592862be27c037292
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2015-08-24 07:05:35 +0200

    Start to us CircleCI to build docker image

--- src/main/java/com/az/ip/api/Application.java
+++ src/main/java/com/az/ip/api/Application.java
@@ -1,14 +1,21 @@
 package com.az.ip.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
+import javax.annotation.PreDestroy;
 import javax.servlet.Filter;
 
 @SpringBootApplication
 public class Application {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    private static final String VERSION = "v1.0.0-M1";
 
     @Bean
     public Filter logFilter() {
@@ -19,7 +26,13 @@ public class Application {
         return filter;
     }
 
+    @PreDestroy
+    public void cleanUp() throws Exception {
+        LOG.info("### AZ-IP-SERVER {} stops", VERSION);
+    }
+
     public static void main(String[] args) {
+        LOG.info("### AZ-IP-SERVER {} starts", VERSION);
         SpringApplication.run(Application.class, args);
     }
 }
